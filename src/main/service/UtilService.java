9ac9280672e7cd9f3102bfa1f9b5e4a6e2e4ef07
emmanuel.roman.santibanez <emmanuel.roman.santibanez@gmail.com>
2021-09-03 19:10:40 -0500

    se agrega el servicio util para funciones generales

---
+++ src/main/service/UtilService.java
@@ -0,0 +1,13 @@
+package main.service;
+
+public class UtilService {
+
+	public boolean isNumeric(String cadena) {
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+	}
+}
